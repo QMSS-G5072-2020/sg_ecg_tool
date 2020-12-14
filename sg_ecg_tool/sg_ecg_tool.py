94e96335bdedfddaba846219d232ce0db96073f2
Kagen <kl3045@columbia.edu>
2020-12-14 20:27:36 +0800

    Function Headers

---
+++ sg_ecg_tool/sg_ecg_tool.py
@@ -0,0 +1,14 @@
+class sg_ecg_tool:
+    """
+    Insert docstrings
+    """
+    def get_employed(self, university_shortname, school, subject, csv=False):
+
+
+    def get_paid(self, university_shortname, school, subject, csv=False):
+
+
+    def get_opening(self, industry_2):
+
+    def get_future_ready(self, university_shortname, school, subject, industry_2, save=False):
+
